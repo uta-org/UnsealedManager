6a485af7fabf11a833ab7b212e05cad742870f5f
Ikillnukes <ikillnukes1@gmail.com>
2019-01-22 21:10:28 +0100

    Added ConsoleDraw to solution thanks to QuickFork

--- UnsealedManager/Program.cs
+++ UnsealedManager/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Text.RegularExpressions;
 
 namespace UnsealedManager
@@ -17,18 +18,25 @@ namespace UnsealedManager
         {
             var files = Directory.GetFiles(StaticPath, "*.cs", SearchOption.AllDirectories);
 
-            Console.WriteLine($"Number of files: {files.Length}");
+            //Console.WriteLine($"Number of files: {files.Length}");
 
             var classes = files.Select(file => GetClassesInFile(file)).SelectMany(c => c).ToList();
 
-            Console.WriteLine($"Number of classes: {classes.Count}");
+            //Console.WriteLine($"Number of classes: {classes.Count}");
 
-            classes.ForEach(tuple => Console.WriteLine($"{tuple.Item1}{(tuple.Item2.HasContent(string.Empty, " ") ? " : " + string.Join(", ", tuple.Item2) + " " : " ")}(on {Path.GetFileName(tuple.Item3)})"));
+            //string fullMsg = string.Join(Environment.NewLine, classes.Select(tuple => $"{tuple.Item1}{(tuple.Item2.HasContent(string.Empty, " ") ? " : " + string.Join(", ", tuple.Item2) + " " : " ")}(on {Path.GetFileName(tuple.Item3)})"));
+            //Console.WriteLine(fullMsg);
 
             //var unsealedClasses = files.Select(file => !classes.Any(cName => Regex.IsMatch(File.ReadAllText(file), $": {cName}")) ? GetClassesInFile(file) : null).SelectMany(c => c).Where(s => !string.IsNullOrEmpty(s));
             var unsealedClasses = classes.Where(c => !classes.Any(subClass => subClass.Item2.Contains(c.Item1))).ToList();
+            //var sealedClasses = classes.Where(c => classes.Any(subClass => subClass.Item2.Contains(c.Item1))).ToList();
 
-            Console.WriteLine($"Unsealed classes count: {unsealedClasses.Count}");
+            StringBuilder sb = new StringBuilder($"Unsealed classes count: {unsealedClasses.Count} ({string.Join(", ", unsealedClasses.Select(c => c.Item1))})");
+            sb.AppendLine($"What do you wish to do with this all {unsealedClasses.Count} unsealed classes?");
+
+            Console.WriteLine(sb.ToString());
+
+            //Console.WriteLine($"Can't be sealed classes count: {sealedClasses.Count}: {string.Join(", ", sealedClasses)}");
 
             //unsealedClasses.ForEach(c => Console.WriteLine(c));
 
@@ -39,12 +47,13 @@ namespace UnsealedManager
         {
             return Regex.Matches(File.ReadAllText(file), $"(public|internal|private) class (?<{ClassName}>(.+?))( : (?<{InheritedClass}>(.+?))|)\n")
                 .Cast<Match>()
-                .Select(m => new Tuple<string, string[], string>(GetGroupValue(m, ClassName), GetGroupValue(m, InheritedClass).CleanLineBreak().Split(','), file));
+                .Select(m => new Tuple<string, string[], string>(GetGroupValue(m, ClassName), GetGroupValue(m, InheritedClass).CleanLineBreak().Split(','), file))
+                .Where(t => !string.IsNullOrEmpty(t.Item1));
         }
 
         private static string GetGroupValue(Match m, string groupName)
         {
-            return m.Groups[groupName].Value.ToString();
+            return m.Groups[groupName].Value.ToString().Trim();
         }
     }
 }
